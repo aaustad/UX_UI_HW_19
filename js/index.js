9a7c7ce0023c13b2ba9c74e19f1489e987f66e0e
aaustad <150180808+aaustad@users.noreply.github.com>
2024-04-14 15:13:24 -0700

    Added jquery

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function()
+    {
+        let props = {
+            "margin-left": '+=500',
+            "opacity": 1
+        }
+        $(".squiggly-container").animate(props, 1000);
+    }
+);
